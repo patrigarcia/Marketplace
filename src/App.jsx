6c8270f99065879286e1dc7d7832c8b036e7fb7b
patrigarcia <gonzalezgpatricia@hotmail.com>
2024-03-30 13:52:18 +0100

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Box, Text } from "@chakra-ui/react";
+
+function App() {
+    return (
+        <>
+            <Box>
+                <Text>Home</Text>
+            </Box>
+        </>
+    );
+}
+
+export default App;
